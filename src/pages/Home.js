b68e2df05b45de01240a5a33ab63317ae87bb33b
pann <23011867@myrp.edu.sg>
2025-01-16 16:00:25 +0800

    c219 lesson 10

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+
+function Home() {
+  // const [workouts, setWorkouts] = useState(null); // local state
+  const { workouts, dispatch } = useWorkoutContext(); // global context state
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/workouts`
+      );
+      const json = await response.json(); // parse JSON response body as JS array of objects
+      if (response.ok) {
+        // setWorkouts(workouts);
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]); // external function must be in dependency array
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+}
+
+export default Home;
